caafc04bfc8e7a8933b5aa8dc005bd347d5e8edc
Osarugue Enehizena <nehirugue@gmail.com>
2024-12-18 18:26:37 +0100

    feat: refactor layout component to manage authentication routing and improve user experience

--- chat-app-firebase/app/_layout.tsx
+++ chat-app-firebase/app/_layout.tsx
@@ -1,14 +1,37 @@
-import { AuthProvider } from "@/context/AuthContext";
-import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { AuthProvider, useAuth } from "../context/AuthContext";
 
-export default function Layout() {
+const InitialLayout = () => {
+  const { user, initialized } = useAuth();
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    if (!initialized) return;
+
+    const inAuthGroup = segments[0] === "(auth)";
+
+    if (user && inAuthGroup) {
+      router.replace("/(tabs)/groups");
+    } else if (!user && !inAuthGroup) {
+      router.replace("/login");
+    }
+  }, [user, initialized]);
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/login" options={{ title: "Login" }} />
+      <Stack.Screen name="(auth)/register" options={{ title: "Register" }} />
+    </Stack>
+  );
+};
+
+export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)/login" options={{ title: "Login" }} />
-        <Stack.Screen name="(auth)/register" options={{ title: "Register" }} />
-      </Stack>
+      <InitialLayout />
     </AuthProvider>
   );
 }
